23d31830d9f889a0a75189a7513416ccc459b87e
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-03-07 02:03:52 +0100

    Setup camera, CameraFollows y salto (PROVISIONAL)

---
+++ BlizzardWizards/Assets/Scripts/Camera/CameraFollows.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime); //lerp es como una transición entre la posición de 2 vectores en un tiempo x
+
+    }
+}
---
+++ BlizzardWizards/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    public int jumpHeight = 150;
+
+    private bool isGrounded;
+    private bool jump;
+    
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        floorMask = LayerMask.GetMask("Floor");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+
+        if (jump && isGrounded)
+        {
+            jump = false;
+            playerRigidbody.AddForce(Vector3.up * jumpHeight);
+        }
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning ()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating (float h, float v)
+    {
+        float forwardAmount = Mathf.Abs(h) + Mathf.Abs(v);
+        anim.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
