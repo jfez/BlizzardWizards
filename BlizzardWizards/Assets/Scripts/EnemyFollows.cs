ef87480fb2d638a9f6fd90cae1915630779898d2
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-04-30 18:49:13 +0200

    zombie y persecución

---
+++ BlizzardWizards/Assets/Scripts/EnemyFollows.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollows : MonoBehaviour
+{
+    GameObject player;
+
+    UnityEngine.AI.NavMeshAgent nav;
+
+    private float distMin;
+    private Vector3 distance;
+
+    private bool moving;
+
+    Rigidbody ZombieRigidbody;
+
+    Animator anim;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        distMin = 5f;
+        anim = GetComponent<Animator>();
+
+        moving = false;
+
+
+
+    }
+
+
+    void Update()
+    {
+        if (!moving)
+        {
+            moving = true;
+        }
+
+        nav.SetDestination(player.transform.position);
+        anim.SetBool("IsMoving", moving);
+
+
+        /*anim.SetBool("IsMoving", moving);
+
+        distance = player.transform.position - transform.position;
+        if (distance.magnitude < distMin && goingHome == false && enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.enabled = true;
+            nav.SetDestination(player.transform.position);
+            moving = true;
+            
+        }
+
+        else if ((distance.magnitude >= distMin && goingHome == false) || enemyHealth.currentHealth < 0 || playerHealth.currentHealth < 0)   //podría cambiarse el playerHealth.currentHealth por el bool de isDead
+        {
+            nav.enabled = false;
+            moving = false;
+        }*/
+
+
+    }
+}
