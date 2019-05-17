d1c3e06ce8c853ad833094b0678612a978ec91d9
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-05-18 01:26:29 +0200

    swap guns

---
+++ BlizzardWizards/Assets/Scripts/Player/changeGun.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeGun : MonoBehaviour
+{
+    public GameObject pistol;
+    public GameObject machineGun;
+    public GameObject shotGun;
+
+    private bool pistoleActive;
+    private bool machineGunActive;
+    private bool shotGunActive;
+
+    private int indexGun;
+    private int numberGuns = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pistol.SetActive(true);
+        pistoleActive = true;
+
+        machineGun.SetActive(false);
+        machineGunActive = false;
+
+        shotGun.SetActive(false);
+        shotGunActive = false;
+
+        indexGun = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if tiene el arma disponible
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !pistoleActive)
+        {
+            pistol.SetActive(true);
+            pistoleActive = true;
+
+            machineGun.SetActive(false);
+            machineGunActive = false;
+
+            shotGun.SetActive(false);
+            shotGunActive = false;
+
+            indexGun = 1;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && !machineGunActive)
+        {
+            pistol.SetActive(false);
+            pistoleActive = false;
+
+            machineGun.SetActive(true);
+            machineGunActive = true;
+
+            shotGun.SetActive(false);
+            shotGunActive = false;
+
+            indexGun = 2;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && !shotGunActive)
+        {
+            pistol.SetActive(false);
+            pistoleActive = false;
+
+            machineGun.SetActive(false);
+            machineGunActive = false;
+
+            shotGun.SetActive(true);
+            shotGunActive = true;
+
+            indexGun = 3;
+        }
+
+        if (Input.mouseScrollDelta.y > 0 && indexGun < numberGuns)
+        {
+            indexGun++;
+
+            if (indexGun == 1)
+            {
+                pistol.SetActive(true);
+                pistoleActive = true;
+
+                machineGun.SetActive(false);
+                machineGunActive = false;
+
+                shotGun.SetActive(false);
+                shotGunActive = false;
+            }
+
+            else if (indexGun == 2)
+            {
+                pistol.SetActive(false);
+                pistoleActive = false;
+
+                machineGun.SetActive(true);
+                machineGunActive = true;
+
+                shotGun.SetActive(false);
+                shotGunActive = false;
+            }
+
+            else if (indexGun == 3)
+            {
+                pistol.SetActive(false);
+                pistoleActive = false;
+
+                machineGun.SetActive(false);
+                machineGunActive = false;
+
+                shotGun.SetActive(true);
+                shotGunActive = true;
+            }
+        }
+
+        else if (Input.mouseScrollDelta.y < 0 && indexGun > 1)
+        {
+            indexGun--;
+
+            if (indexGun == 1)
+            {
+                pistol.SetActive(true);
+                pistoleActive = true;
+
+                machineGun.SetActive(false);
+                machineGunActive = false;
+
+                shotGun.SetActive(false);
+                shotGunActive = false;
+            }
+
+            else if (indexGun == 2)
+            {
+                pistol.SetActive(false);
+                pistoleActive = false;
+
+                machineGun.SetActive(true);
+                machineGunActive = true;
+
+                shotGun.SetActive(false);
+                shotGunActive = false;
+            }
+
+            else if (indexGun == 3)
+            {
+                pistol.SetActive(false);
+                pistoleActive = false;
+
+                machineGun.SetActive(false);
+                machineGunActive = false;
+
+                shotGun.SetActive(true);
+                shotGunActive = true;
+            }
+        }
+
+    }
+}
