f26c9a839e6d449a6f8e4cf8d37d581a93529f15
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-05-26 13:24:23 +0200

    Menú Inicial

---
+++ BlizzardWizards/Assets/Scripts/Camera/CameraMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenu : MonoBehaviour
+{
+    private float timer;
+    private bool paneo;
+    private float offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        paneo = false;
+        offset = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        offset = offset - 0.01f;
+        
+
+        if (timer > 5)
+        {
+            paneo = !paneo;
+            timer = 0;
+            offset = 1;
+        }
+
+
+        if (!paneo)
+        {
+           
+
+            transform.Translate(Vector3.forward * Time.deltaTime * offset);
+
+
+        }
+
+        else
+        {
+            
+
+            transform.Translate(-Vector3.forward * Time.deltaTime * offset);
+        }
+        
+    }
+}
